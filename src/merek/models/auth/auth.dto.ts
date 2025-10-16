bcf1c28b52ef8bec4d9a7e3ab3546823f7672001
Ardhan Nur Urfan <ardhanurfan@gmail.com>
2025-10-16 07:08:07 +0700

    chore: delete unused login dto

--- src/merek/models/auth/auth.dto.ts
+++ src/merek/models/auth/auth.dto.ts
@@ -8,21 +8,4 @@ export class LoginDto {
   @IsString()
   @IsNotEmpty()
   password: string;
-
-  // @IsString()
-  // @IsNotEmpty()
-  // captchaAnswer: string;
-
-  // @IsString()
-  // @IsNotEmpty()
-  // csrfToken: string;
-
-  // @IsString()
-  // @IsNotEmpty()
-  // captchaKey: string;
-
-  // @IsArray()
-  // @ArrayNotEmpty()
-  // @IsString({ each: true })
-  // cookies: string[];
 }
